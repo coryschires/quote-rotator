c1efc063a725015d14de4cd8358f70da8a018193
Cory Schires <coryschires@gmail.com>
2011-01-25 14:56:53 -0600

    New feature: The ability to randomize the first quote shown. Updated the readme to reflect this change.
    Also. Version Bump.

--- jquery.quote_rotator.js
+++ jquery.quote_rotator.js
@@ -1,48 +1,52 @@
-// Simple quote rotation plugin written for in3media/radio-info.com
-// Cory Schires - Feb 3, 2010
-// 
-// Instructions:
-// Simply apply this function to any list element. It will rotate through each list items 
-// with a nice fade effect. You may nest any type of element you like inside a given list item.
-//
-//     $('ul#quotes').quote_rotator();
-//
-// Configuration:
-// You can set the rotation_speed in milliseconds. Minimum rotation speed is 2000.  
-//
-//     $('ul#quotes').quote_rotator({ 
-//          rotation_speed: 7000    // defaults to 5000
-//          pause_on_hover: false   // defauts to true
-//      });
-//
+/**
+ * Quote Rotator - Simple jQuery plugin which cause a set of list
+ * items to fade-in and fade-out. 
+ * 
+ * Homepage: http://coryschires.com/jquery-quote-rotator-plugin/
+ * Source Code: https://github.com/coryschires/quote-rotator
+ * 
+ * Copyright (c) 2011 Cory Schires (coryschires.com)
+ * Dual licensed under the MIT and GPL licenses:
+ * http://www.opensource.org/licenses/mit-license.php
+ * http://www.gnu.org/licenses/gpl.html
+ *
+ * Version: 0.1.0
+ */
 
 (function($) {
 
-	$.quote_rotator = {
-		defaults: {
-			rotation_speed: 5000,
-			pause_on_hover: true
-		}		
-	}
+  $.quote_rotator = {
+    defaults: {
+      rotation_speed: 5000,
+      pause_on_hover: true,
+      randomize_first_quote: false
+    }
+  }
 
     $.fn.extend({
         quote_rotator: function(config) {
-			
-			var config = $.extend({}, $.quote_rotator.defaults, config);
-			
+
+            var config = $.extend({}, $.quote_rotator.defaults, config);
+
             return this.each(function() {
                 
                 // cache for selector performance
                 var quote_list = $(this);
+                var list_items = quote_list.find('li');
                 
                 // prevent dummies from setting the rotation speed too fast
-				var rotation_speed = config.rotation_speed < 2000 ? 2000 : config.rotation_speed;                   
+                var rotation_speed = config.rotation_speed < 2000 ? 2000 : config.rotation_speed;                   
 
                 // hide all the quotes
-                quote_list.find('li').hide();
+                list_items.hide();
 
-                // apply active class to first quote unless already applied elsewhere
-                if (! (quote_list.find('li.active')).length ) {
+                // apply active to random quote if specified
+                if (config.randomize_first_quote) {
+                  var random = Math.floor( Math.random() * (list_items.length) );
+                  $(list_items[random]).addClass('active');
+
+                // otherwise apply active to first quote unless already applied in the html
+                } else if (! (quote_list.find('li.active')).length ) {
                     $('li:first', quote_list).addClass('active');
                 };
                 
@@ -62,25 +66,25 @@
                 // rotate quotes
                 setInterval(function(){
                     if (rotation_active) {
-                                            
+                        
                         var active_quote = quote_list.find('li.active');        
                         var next_quote =  active_quote.next().length ? active_quote.next() : quote_list.find('li:first');
 
                         // rotate quotes with fade effect
                         active_quote.animate({
                             opacity: 0 // fade out active quote
-                        }, 1000, function() {                        
+                        }, 1000, function() {
                             active_quote.hide().css('opacity', 1); // hide and reset opacity
                             next_quote.fadeIn(1000); // fade in next quote
                         });
-
+                        
                         // swap the class to prepare for the next fade effect interal
-                        active_quote.removeClass('active');         
+                        active_quote.removeClass('active');
                         next_quote.addClass('active');
                     };
-
-                }, rotation_speed);  
-                                
+                    
+                }, rotation_speed);
+                
             })
         }
     })
